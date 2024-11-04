c024a9598e30c90bd07ff21178991377caff8bda
NathyEluna <nathcsr@gmail.com>
2024-11-04 23:57:36 +0100

    He terminado la practica

--- Practica403/bibliotecas/biblioteca.js
+++ Practica403/bibliotecas/biblioteca.js
@@ -1,21 +1,56 @@
 "use strict";
 
-export const crearTabla = () => {
-    const lienzo = document.getElementById("lienzo");
+//Función para crear la tabla(lienzo).
+export const crearTabla = (lienzo) => {
+    //Antes había intentado crear la tabla directamente en lienzo con innerHTML pero solo <table></table> era creado.
+    //Entonces yo tuve que crear esa variable para primero crear la tabla y entonces añadirla a el elemento DIV.
+    let tabla = `<table id="lienzo-tabla">`;
 
-    lienzo.innerHTML += `<table>`;
-
-    for (let i = 0; i < 60; i++) {
-        lienzo.innerHTML += `<tr>`;
-        for (let j = 0; j < 50; j++) {
-            lienzo.innerHTML += `<td></td>`;
+    //He utilizado for para la creación de la tabla con 50 de ancho y 60 de largo.
+    for (let i = 0; i < 50; i++) {
+        tabla += `<tr>`;
+        for (let j = 0; j < 60; j++) {
+            tabla += `<td class="lienzo-celda"></td>`;
         };//for j.
         
-        lienzo.innerHTML += `</tr>`;
+        tabla += `</tr>`;
     };//for i.
-    lienzo.innerHTML += `</table>`;
+    tabla += `</table>`;
+
+    //Después añadimos la tabla directament al div.
+    lienzo.innerHTML = tabla;
 };
 
-//lienzo.innerHTML += ``;
+//Función para seleccionar los colores.
+//Al final he intentado de diversas maneras, pero la única que ha funcionado ha sido utilziando el callback. Tuve que buscar para que servía el callback y como utilizarlo.
+export const seleccionarColor = (callback) => {
+    const colores = document.getElementById("pintura-tabla");
+    
 
-//columnas = 50 filas = 60
+    colores.onclick = (event) => {
+        const celda = event.target;
+
+        if(celda.tagName === "TD"){
+            //Removemos la clase "border" de las celdas que ya la tenga.
+            const seleccionado = document.getElementsByClassName("border");
+            Array.from(seleccionado).forEach((celda) => {
+                celda.classList.remove("border");
+            });
+
+            //Actualizamos la clase del color y llamamos al callback.
+            const nuevoColor = celda.className; //Cambiamos el color para el seleccionado.
+            
+            //Llamamos a la función de callback para actualizar el color.
+            callback(nuevoColor);
+            celda.classList.add("border");
+        };
+    };
+};
+
+//Función para borrar el lienzo.
+export const borrarLienzo = () => {
+    //Aquí buscamos por las celdas del lienzo y cambiamos el color de fondo para blanco.
+    Array.from(document.getElementsByClassName("lienzo-celda")).forEach(celda => {
+        celda.className = "lienzo-celda white";
+    });
+};
--- Practica403/script.js
+++ Practica403/script.js
@@ -1,6 +1,41 @@
 "use strict";
-import { crearTabla } from "./bibliotecas/biblioteca.js";
+import { crearTabla, seleccionarColor, borrarLienzo } from "./bibliotecas/biblioteca.js";
+//Al final he conseguido que pinte al mantener presionado el ratón y no clicando para empezar a pintar y parar.
 
 window.onload = () => {
-    crearTabla();
+    //Guardamos la referencia del elemento donde se creará la tabla.
+    const lienzo = document.getElementById("lienzo");
+    //Variable para verificar si estamos pintando o no.
+    let pintando = false;
+    //Color inicial.
+    let colorSeleccionado = "white";
+
+    //Llamada a la función para inicializar el selector de colores.
+    seleccionarColor((nuevoColor) =>{
+        //Actualizamos el color seleccionado.
+        colorSeleccionado = nuevoColor;
+    });
+    //Llamada a la función para crear la tabla.
+    crearTabla(lienzo);
+
+    //Empezar a pintar.
+    lienzo.addEventListener("mousedown", () => {
+        pintando = true;
+    }, false);
+    //Parar de pintar.
+    lienzo.addEventListener("mouseup", () => {
+        pintando = false;
+    }, false);
+    //Pintar al mover el ratón.
+    lienzo.addEventListener("mouseover", (event) => {
+        if(pintando && event.target.tagName === "TD"){
+            //Aplica el color seleccionado.
+            event.target.className = `lienzo-celda ${colorSeleccionado}`;
+        };
+    }, false);
+
+    //Evento del botón de borrar el lienzo.
+    document.getElementById("borrar").addEventListener("click", () =>{
+        borrarLienzo();
+    }, false);
 };//window onload.
